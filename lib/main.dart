50994c2eff7a0eb25e0561a41ea84cb5aaf16d4b
TanyaDinesh00 <tanyadinesh21@gmail.com>
2020-07-24 17:10:21 +0530

    Initial commit.

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,6 @@ class MyApp extends StatelessWidget {
         "/": (context) => SafeArea(
               child: WebviewScaffold(
                 url: url,
-//              appBar: AppBar(
-//                title: Text("WebView"),
-//              ),
                 withJavascript: true,
                 withLocalStorage: true,
                 withZoom: true,
@@ -31,56 +28,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-//
-//class Home extends StatefulWidget {
-//  @override
-//  HomeState createState() => HomeState();
-//}
-//
-//class HomeState extends State<Home> {
-//  final webView = FlutterWebviewPlugin();
-//  var controller = TextEditingController(text: url);
-//
-//  @override
-//  void initState() {
-//    super.initState();
-//
-//    webView.close();
-//    controller.addListener(() {
-//      url = controller.text;
-//    });
-//  }
-//
-//  @override
-//  void dispose() {
-//    webView.dispose();
-//    controller.dispose();
-//    super.dispose();
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//        appBar: AppBar(
-//          title: Text("WebView"),
-//        ),
-//        body: Center(
-//          child: Column(
-//            children: <Widget>[
-//              Container(
-//                padding: EdgeInsets.all(10.0),
-//                child: TextField(
-//                  controller: controller,
-//                ),
-//              ),
-//              RaisedButton(
-//                child: Text("Open Webview"),
-//                onPressed: () {
-//                  Navigator.of(context).pushNamed("/webview");
-//                },
-//              )
-//            ],
-//          ),
-//        ));
-//  }
-//}
